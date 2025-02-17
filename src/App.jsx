55801cee230a5edd1fc1a90f5374f70458870561
satinder36 <ssatinder1996@gmail.com>
2025-02-17 23:41:59 +0530

    router config

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Body from "./component/Body";
+import Login from "./pages/auth/Login";
+import Profile from "./pages/user/Profile";
+
 function App() {
   return (
     <>
-      <div className="navbar bg-primary			">
-        <div className="flex-1">
-          <a className="btn btn-ghost text-xl font-bold text-white">
-            Sa-Tinder
-          </a>
-        </div>
-        <div className="flex-none gap-2">
-          <div className="dropdown dropdown-end mr-3">
-            <div
-              tabIndex={0}
-              role="button"
-              className="btn btn-ghost btn-circle avatar"
-            >
-              <div className="w-10 rounded-full">
-                <img
-                  alt="Tailwind CSS Navbar component"
-                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
-                />
-              </div>
-            </div>
-            <ul
-              tabIndex={0}
-              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
-            >
-              <li>
-                <a className="justify-between">
-                  Profile
-                  <span className="badge">New</span>
-                </a>
-              </li>
-              <li>
-                <a>Settings</a>
-              </li>
-              <li>
-                <a>Logout</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
